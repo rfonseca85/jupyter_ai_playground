af235ca237b9da9335a424193daa9f403e0c2e29
rafael.fonseca <rfonseca85@yahoo.ca>
2023-11-05 23:24:03 -0500

    adding new swap tests

--- code/ai/face_swap_ai/swap_face.py
+++ code/ai/face_swap_ai/swap_face.py
@@ -1,15 +1,7 @@
 import cv2
 import matplotlib.pyplot as plt
-import insightface
-from insightface.app import FaceAnalysis
 
-
-def swap_n_show(img1, img1_pos, img2, img2_pos, plot_before=True, plot_after=True):
-    # Step 1 Detecting Faces
-    app = FaceAnalysis(name='buffalo_l')
-    app.prepare(ctx_id=0, det_size=(640, 640))
-    swapper = insightface.model_zoo.get_model('model/inswapper_128.onnx', download=False, download_zip=False)
-
+def swap_n_show(img1, img1_pos, img2, img2_pos, app, swapper, plot_before=True, plot_after=True):
     # read the 2 images
     img1 = cv2.imread('img/' + img1)
     img2 = cv2.imread('img/' + img2)
@@ -35,13 +27,19 @@ def swap_n_show(img1, img1_pos, img2, img2_pos, plot_before=True, plot_after=Tru
         plt.imshow(img2[bbox[1]:bbox[3], bbox[0]:bbox[2], ::-1])
         plt.show()
 
-
-
-    img_final = img2.copy()
+    img1 = img1.copy()
+    img2 = img2.copy()
 
     if plot_after:
-        img_final = swapper.get(img2, face1, face2, paste_back=True)
+        img2 = swapper.get(img2, face1, face2, paste_back=True)
         fig, axs = plt.subplots(1, 2, figsize=(10, 5))
-        axs[0].imshow(img_final[:, :, ::-1])
+        axs[0].imshow(img2[:, :, ::-1])
         axs[0].axis('off')
 
+        face_final = app.get(img2)[img2_pos]
+        bbox = face_final['bbox']
+        bbox = [int(b) for b in bbox]
+        plt.imshow(img2[bbox[1]:bbox[3], bbox[0]:bbox[2], ::-1])
+        plt.show()
+
+    return img1, img2
